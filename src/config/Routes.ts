a12aa10a73d28144ab893f613ff055fb5b536280
Kevin Stoop <433243@student.fontys.nl>
2022-07-06 22:47:58 +0200

    added homepage menu
    Signed-off-by: Kevin Stoop <433243@student.fontys.nl>

--- src/config/Routes.ts
+++ src/config/Routes.ts
@@ -1,10 +1,14 @@
 import IRoute from "../interfaces/Routes";
 import Home from "../pages/Home";
+import Resume from "../pages/Resume";
 
 const routes: IRoute[] = [
     {
         path: '/',
         element: Home
+    }, {
+        path: '/resume',
+        element: Resume
     }
 ];
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import STYLE from '../assets/scss/home.module.scss';
 
 type Props = {}
@@ -13,6 +14,42 @@ const Home = (props: Props) => {
                     <h2>
                         passionate about digital products that help people experience everyday life, not endure it.
                     </h2>
+                </div>
+            </div>
+            <div className={STYLE['work']}>
+                <div className={STYLE['work--grid']}>
+                    <div className={STYLE['work--selected']}>
+                        <div className={STYLE['work--selected-title']}>
+                            Work
+                        </div>
+                        <div className={STYLE['work--selected-bar']}>
+                            <div className={STYLE['work--selected-bar-animation']}></div>
+                        </div>
+                    </div>
+                    <div className={STYLE['work--social']}>
+                        <Link to="/resume" className={STYLE['work--social-link']}>
+                            <span>
+                                Resume
+                            </span>
+                            <div className={STYLE['work--social-link--icon']}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                    <line x1="5" y1="12" x2="19" y2="12"></line>
+                                    <polyline points="12 5 19 12 12 19"></polyline>
+                                </svg>
+                            </div>
+                        </Link>
+                        <a href="https://www.linkedin.com/in/kevinstoop/" className={STYLE['work--social-link']} target={'_blank'} rel="nofollow">
+                            <span>
+                                LinkedIn
+                            </span>
+                            <div className={STYLE['work--social-link--icon']}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                    <line x1="5" y1="12" x2="19" y2="12"></line>
+                                    <polyline points="12 5 19 12 12 19"></polyline>
+                                </svg>
+                            </div>
+                        </a>
+                    </div>
                 </div>
             </div>
         </>
