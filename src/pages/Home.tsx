7627c72f668bb52f0ca0aa3bff1c27456cad49d9
Kevin Stoop <433243@student.fontys.nl>
2022-07-12 22:43:38 +0200

    imported images for build
    Signed-off-by: Kevin Stoop <433243@student.fontys.nl>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,9 @@
 import { Link } from 'react-router-dom';
+import ImageBetonboringengeffen from '../assets/images/projects/betonboringengeffen/index.webp';
+import ImageCRM from '../assets/images/projects/crm/index.webp';
+import ImagePraktijk212 from '../assets/images/projects/praktijk212/index.webp';
+import ImageScabtik2022 from '../assets/images/projects/scabtik2022/index.webp';
+import ImageVVD from '../assets/images/projects/vvd/index.webp';
 import STYLE from '../assets/scss/home.module.scss';
 import CaseItem from '../components/CaseItem';
 
@@ -66,13 +71,13 @@ const Home = (props: Props) => {
             </div>
             <div className={STYLE['work--projects']}>
                 <div className={STYLE['work--projects-grid']}>
-                    <CaseItem title="CRM-system" description='Helps you to manage your customer data.' image='/src/assets/images/projects/crm/index.webp' link="/crm-system" gradientBeginColor="rgb(1, 87, 208)" gradientEndColor="rgb(255,255,255)" />
+                    <CaseItem title="CRM-system" description='Helps you to manage your customer data.' image={ImageCRM} link="/crm-system" gradientBeginColor="rgb(1, 87, 208)" gradientEndColor="rgb(255,255,255)" />
 
-                    <CaseItem title="Scabtik rebranding" description='Website rebranding for an upcoming Hardstyle artist.' image='/src/assets/images/projects/scabtik2022/index.webp' link="/scabtik-2022" gradientBeginColor="rgb(171, 129, 95)" gradientEndColor="rgb(30, 30, 30)" />
+                    <CaseItem title="Scabtik rebranding" description='Website rebranding for an upcoming Hardstyle artist.' image={ImageScabtik2022} link="/scabtik-2022" gradientBeginColor="rgb(171, 129, 95)" gradientEndColor="rgb(30, 30, 30)" />
 
-                    <CaseItem title="VVD-kieswijzer" description='Personal election guide for the municipal elections of a political party.' image='/src/assets/images/projects/vvd/index.webp' link="/vvd" gradientBeginColor="rgb(255, 100, 0)" gradientEndColor="rgb(10, 44, 202)" />
+                    <CaseItem title="VVD-kieswijzer" description='Personal election guide for the municipal elections of a political party.' image={ImageVVD} link="/vvd" gradientBeginColor="rgb(255, 100, 0)" gradientEndColor="rgb(10, 44, 202)" />
 
-                    <CaseItem title="Praktijk 212" description='Website for a dentist in Amsterdam.' image='/src/assets/images/projects/praktijk212/index.webp' link="/praktijk212" gradientBeginColor="rgb(240, 201, 171)" gradientEndColor="rgb(213, 228, 244)" />
+                    <CaseItem title="Praktijk 212" description='Website for a dentist in Amsterdam.' image={ImagePraktijk212} link="/praktijk212" gradientBeginColor="rgb(240, 201, 171)" gradientEndColor="rgb(213, 228, 244)" />
 
                     <CaseItem title="RFC Maastricht" description='Website for a medicine contest in Maastricht.' image='' link="/rfc-maastricht" gradientBeginColor="" gradientEndColor="" />
 
@@ -98,7 +103,7 @@ const Home = (props: Props) => {
 
                     <CaseItem title="Gewoon Samen" description='Website for helping people with physical disabilities.' image='' link="/gewoon-samen" gradientBeginColor="" gradientEndColor="" />
 
-                    <CaseItem title="Betonboringen Geffen" description='Website for a concrete drilling company.' image='/src/assets/images/projects/betonboringengeffen/index.webp' link="/betonboringen-geffen" gradientBeginColor="rgb(0, 89, 68)" gradientEndColor="rgb(191, 207, 220)" />
+                    <CaseItem title="Betonboringen Geffen" description='Website for a concrete drilling company.' image={ImageBetonboringengeffen} link="/betonboringen-geffen" gradientBeginColor="rgb(0, 89, 68)" gradientEndColor="rgb(191, 207, 220)" />
 
                     <CaseItem title="Red Mijn Scriptie" description='Website for help writing a thesis.' image='' link="/red-mijn-scriptie" gradientBeginColor="" gradientEndColor="" />
 
